4a3f51117e810177c468c7fb353f39f45490c57d
j-shilling <shilling.jake@gmail.com>
2019-02-06 08:08:09 -0500

    Adding solution

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,37 @@
 class OrdersController < ApplicationController
+  before_action :get_order, only: [:edit, :update, :show]
 
   def index
     @orders = Order.all
   end
 
   def new
+    @order = Order.new
+    @order.customer = Customer.new
+    @order.item = Item.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      @order.customer = Customer.new if @order.customer == nil
+      @order.item = Item.new if @order.item == nil
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @order.update(order_params)
+    if @order.valid?
+      redirect_to order_path(@order)
+    else
+      render :edit
+    end
   end
 
   def show
@@ -25,4 +43,7 @@ class OrdersController < ApplicationController
     params.require(:order).permit(:quantity, :item_id, :customer_id, item_attributes: [:name, :description, :price], customer_attributes: [:first_name, :last_name, :email])
   end
 
+  def get_order
+    @order = Order.find(params[:id])
+  end
 end
