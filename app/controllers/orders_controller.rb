642fbac147f3a83891008c38dae26478417da5da
j-shilling <shilling.jake@gmail.com>
2019-02-06 07:45:06 -0500

    Adding starter code

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :item_id, :customer_id, item_attributes: [:name, :description, :price], customer_attributes: [:first_name, :last_name, :email])
+  end
+
+end
