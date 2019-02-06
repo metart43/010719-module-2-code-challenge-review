a56eba07d6bba971a6394c375e208e3cb8ef7f03
j-shilling <shilling.jake@gmail.com>
2019-02-06 08:06:53 -0500

    Removing description from items

--- db/migrate/02_create_items.rb
+++ db/migrate/02_create_items.rb
@@ -3,7 +3,6 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :description
       t.float :price
 
       t.timestamps
